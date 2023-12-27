72dd763e157f584ff017852722c902916f573341
Shin <shin@ShinnoMacBook-Air.local>
2023-12-27 13:52:46 +0900

    最終課題終了

--- sidebar.php
+++ sidebar.php
@@ -2,15 +2,14 @@
 
 <div class="p-menu__bg"></div>
 
-<nav class="l-nav p-wrapper__aside">
+<nav class="l-nav main-menu p-wrapper__aside">
         <button class="c-btn_menu-close"></button><!-- /.c-btn_menu-close -->
         
         <h5 class="c-title-menu p-title__pc-menu">Menu</h5>
 
         <div class="p-aside-container">
 
-            <!-- <?php wp_nav_menu(); ?> -->
-            <?php wp_nav_menu( array('theme_location' => 'mainmenu')); ?>
+            <?php wp_nav_menu( array('theme_location' => 'main-menu')); ?>
 
     
         </div><!--p-aside-container-->
